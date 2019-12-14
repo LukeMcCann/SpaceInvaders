8eaae42a4246209dd1e88f1941d884afb8d70ffe
Luke McCann <developer-lukemccann@outlook.com>
2019-12-14 20:53:50 +0000

    Adds first enemy

--- main.py
+++ main.py
@@ -15,11 +15,28 @@ pygame.display.set_icon(icon)
 
 # Player
 playerImg = pygame.image.load("images/player.png")
-playerX = WIN_WIDTH/2-25
-playerY = WIN_HEIGHT/2+180
 
-def player(x,y):
-    window.blit(playerImg, [x,y])
+playerX = WIN_WIDTH / 2 - 25
+playerY = WIN_HEIGHT / 2 + 180
+
+PLAYER_SPEED = 0.3
+
+
+def player(x, y):
+    window.blit(playerImg, [x, y])
+
+
+# Invader
+invaderImg = pygame.image.load("images/invader.png")
+invaderX = WIN_HEIGHT / 2
+invaderY = WIN_WIDTH / 2
+
+INVADER_SPEED = 0.1
+
+
+def invader(x, y):
+    window.blit(invaderImg, [x, y])
+
 
 # Game Loop
 RUNNING = True
@@ -28,12 +45,19 @@ while RUNNING:
         if event.type == pygame.QUIT:
             RUNNING = False
 
+    window.fill([0, 0, 51])
+
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_LEFT:
-            playerX -= 0.05;
+            playerX -= PLAYER_SPEED
         if event.key == pygame.K_RIGHT:
-            playerX += 0.05
+            playerX += PLAYER_SPEED
 
-    # window.fill((0,0,0))
-    player(playerX,playerY)
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= WIN_WIDTH - 64:
+        playerX = WIN_WIDTH - 64
+
+    player(playerX, playerY)
+    invader(invaderX, invaderY)
     pygame.display.update()
