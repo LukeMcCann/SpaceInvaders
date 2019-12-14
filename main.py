94bc6a713455e805e35086a8942df9f941e0fe28
Luke McCann <developer-lukemccann@outlook.com>
2019-12-14 20:04:35 +0000

    Adds player graphics

--- main.py
+++ main.py
@@ -3,12 +3,24 @@ import pygame
 # initialize pygame
 pygame.init()
 
-window = pygame.display.set_mode((800, 600))
+WIN_WIDTH = 800
+WIN_HEIGHT = 600
+
+window = pygame.display.set_mode([WIN_WIDTH, WIN_HEIGHT])
 
 # Title and icon
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('images/ufo.png')
 pygame.display.set_icon(icon)
+
+# Player
+playerImg = pygame.image.load("images/player.png")
+playerX = WIN_WIDTH/2-25
+playerY = WIN_HEIGHT/2+180
+
+def player():
+    window.blit(playerImg, [playerX, playerY])
+
 
 # Game Loop
 RUNNING = True
@@ -16,3 +28,7 @@ while RUNNING:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             RUNNING = False
+
+    # window.fill((0,0,0))
+    player()
+    pygame.display.update()
